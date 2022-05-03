205558927035c8173acd4a1e27f43439c83decd0
zzh <240918836@qq.com>
2022-05-03 08:36:28 +0800

    用户注册表单6.3

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,6 +41,6 @@ class User extends Authenticatable
     public function gravatar($size = '100')
     {
         $hash = md5(strtolower(trim($this->attributes['email'])));
-        return "http://www.gravatar.com/avatar/$hash?s=$size";
+        return "https://cdn.v2ex.com/gravatar/$hash?s=$size";
     }
 }
